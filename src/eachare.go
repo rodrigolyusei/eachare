a0c21384d760704e8f09cf75d894937305b58f9d
Dimitri Prado <dimitriprado@gmail.com>
2025-03-23 21:14:09 -0300

    feat: adição de um arquivo test.sh para facilitar o startup do programa e criação do getArgs, função para pegar os argumentos.

--- src/eachare.go
+++ src/eachare.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-    x := strings.Split(os.Args[1], ":")
-
-	addr := x[0]
-	port := x[1]
-	neighbors := os.Args[2]
-	shared := os.Args[3]
+	addr, port, neighbors, shared := getArgs(os.Args)
 
 	fmt.Println("Endereço:", addr)
 	fmt.Println("Porta:", port)
 	fmt.Println("Vizinhos:", neighbors)
 	fmt.Println("Diretório Compartilhado:", shared)
+}
+
+func getArgs(args []string) (string, string, string, string) {
+	x := strings.Split(args[1], ":")
+	return x[0], x[1], os.Args[2], os.Args[3]
 }
