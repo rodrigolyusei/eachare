a5ecf91be1e87f8a4054a3d05ff58b6f255c8b31
Rodrigo Lyusei <rodrigo.lyusei@gmail.com>
2025-03-24 16:58:35 -0300

    add: verificação dos parâmetros de entrada

--- src/eachare.go
+++ src/eachare.go
@@ -7,8 +7,21 @@ import (
 )
 
 func main() {
+	// Verifica se a quantidade de parâmetros, endereço e porta de entrada é válida
+	if len(os.Args) < 4 || len(os.Args) > 4 {
+		fmt.Println("Parâmetros de entrada inválidos, por favor, siga o formato abaixo:")
+		fmt.Println("./eachare <endereço>:<porta> <vizinhos> <diretório compartilhado>")
+		os.Exit(1)
+	} else if !strings.Contains(os.Args[1], ":") {
+		fmt.Println("Endereço e porta inválidos, por favor, siga o formato abaixo:")
+		fmt.Println("./eachare <endereço>:<porta> <vizinhos> <diretório compartilhado>")
+		os.Exit(1)
+	}
+
+	// Recebe os parâmetros de entrada
 	addr, port, neighbors, shared := getArgs(os.Args)
 
+	// Imprime os parâmetros de entrada
 	fmt.Println("Endereço:", addr)
 	fmt.Println("Porta:", port)
 	fmt.Println("Vizinhos:", neighbors)
