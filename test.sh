1b2615a48b90c4cda36b7548624136ce1f77e3cf
Dimitri Prado <dimitriprado@gmail.com>
2025-03-24 21:34:29 -0300

    fix: modificação do gitigore para contemplar um direotiro temp e modifcação do test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
+cd src
 
-if [ -z "$1" ]; then
-    set -- "127.0.0.1:8080" "neighbors" "shared"
-fi
-# Compila o arquivo eachare.go
-go run src/eachare.go "$1" "$2" "$3" "$4"
-# Define argumentos padrão para testes
+go run eachare.go 127.0.0.1:8080 neighbors shared
