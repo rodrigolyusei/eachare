659d1839eb4abe67bd9002cf01bf84ff5cc627a8
Dimitri Prado <dimitriprado@gmail.com>
2025-03-24 21:34:53 -0300

    fix: colocando validaçào de argumentos de entrada dentro da função getArgs.

--- src/eachare.go
+++ src/eachare.go
@@ -9,18 +9,7 @@ import (
 )
 
 func main() {
-	// Verifica se a quantidade de parâmetros, endereço e porta de entrada é válida
-	if len(os.Args) < 4 || len(os.Args) > 4 {
-		fmt.Println("Parâmetros de entrada inválidos, por favor, siga o formato abaixo:")
-		fmt.Println("./eachare <endereço>:<porta> <vizinhos> <diretório compartilhado>")
-		os.Exit(1)
-	} else if !strings.Contains(os.Args[1], ":") {
-		fmt.Println("Endereço e porta inválidos, por favor, siga o formato abaixo:")
-		fmt.Println("./eachare <endereço>:<porta> <vizinhos> <diretório compartilhado>")
-		os.Exit(1)
-	}
-
-	// Recebe os parâmetros de entrada
+		// Recebe os parâmetros de entrada
 	addr, port, neighbors, shared := getArgs(os.Args)
 
 	// Imprime os parâmetros de entrada
@@ -28,13 +17,23 @@ func main() {
 	fmt.Println("Porta:", port)
 	fmt.Println("Vizinhos:", neighbors)
 	fmt.Println("Diretório Compartilhado:", shared)
-
+	
 	// Inicializa o servidor
 	comm := commands.GetCommand()
 	fmt.Println("Valor escolhido: " + comm)
 }
 
 func getArgs(args []string) (string, string, string, string) {
+	if len(args) != 4 {
+		fmt.Println("Parâmetros de entrada inválidos, por favor, siga o formato abaixo:")
+		fmt.Println("./eachare <endereço>:<porta> <vizinhos> <diretório compartilhado>")
+		os.Exit(1)
+	} else if !strings.Contains(args[1], ":") {
+		fmt.Println("Endereço e porta inválidos, por favor, siga o formato abaixo:")
+		fmt.Println("./eachare <endereço>:<porta> <vizinhos> <diretório compartilhado>")
+		os.Exit(1)
+	}
+
 	x := strings.Split(args[1], ":")
 	return x[0], x[1], args[2], args[3]
 }
